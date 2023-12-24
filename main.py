0be5a2e0b9d31bc9eb63ee0a3669cb4b4212491c
Tembo-1 <tembo535@gmail.com>
2023-12-24 10:22:47 +0300

    add write in json

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
 from perceptron import Perceptron
 from population import *
+import json
 
 population = Population ()
 
 # population.Selection()
 
-evolution = 100
+evolution = 70
 
 for i in range(evolution):
     population.Selection()
     population.Reproduction()
     population.Mutate()
+
+data = population.person
+
+winner:Population
+max = 0
+
+for i in data:
+    if (i.score > max):
+        winner = i
+
+data = []
+
+for i in range(4):
+    for j in range(winner.in_sloy[i]):
+        data.append({"sloy": i, "porog" : winner.sloy[i][j].porog, "weight" : winner.sloy[i][j].weight})
+     
+
+with open('data.json', 'w') as f:
+    json.dump(data, f)
 
 
 
